7944b43d346f162ecf935fcfac5e2224c0a95635
qianxiaoxiao <909263817@qq.com>
2016-09-14 17:25:19 +0800

    增加rest方式调用

--- dubbo-hello-provider/src/main/java/cn/qianxiaoxiao/dubbo/hello/provider/HelloServiceImpl.java
+++ dubbo-hello-provider/src/main/java/cn/qianxiaoxiao/dubbo/hello/provider/HelloServiceImpl.java
@@ -1,10 +1,21 @@
 package cn.qianxiaoxiao.dubbo.hello.provider;
 
 import cn.qianxiaoxiao.dubbo.hello.api.HelloService;
+import com.alibaba.dubbo.rpc.protocol.rest.support.ContentType;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import javax.ws.rs.ext.*;
 
+@Path("hello")
+@Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_XML})
+@Produces({ContentType.APPLICATION_JSON_UTF_8, ContentType.TEXT_XML_UTF_8})
 public class HelloServiceImpl implements HelloService {
-
-    public String hello(String name) {
+	
+	@GET
+	@Path("/say/{name}")
+	@Produces({MediaType.APPLICATION_JSON})
+    public String hello(@PathParam("name") String name) {
+		System.out.println(name);
         return "hello " + name;
     }
 }
